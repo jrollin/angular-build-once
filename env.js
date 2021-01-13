d1bf74b4b10f14cb46a05f210a40d91f872307c5
Julien ROLLIN <1086565+jrollin@users.noreply.github.com>
2021-01-13 17:41:45 +0100

    Updates

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 (function (window) {
     window.__env = window.__env || {};
-    window.__env.apiUrl = 'http://localhost:8080';
-    window.__env.envName = 'local';
+    window.__env.apiUrl = 'http://github.whatever';
+    window.__env.envName = 'github';
 }(this));
