c5d83854291fe65b5901ad48396e4634ecce0586
Julien ROLLIN <1086565+jrollin@users.noreply.github.com>
2021-01-13 17:43:48 +0100

    refactor: renamed app and add npm packages

--- dist/cd-env-angular/env.js
+++
@@ -1,5 +0,0 @@
-(function (window) {
-    window.__env = window.__env || {};
-    window.__env.apiUrl = 'http://localhost:8080';
-    window.__env.envName = 'local';
-}(this));
--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('cd-env-angular app is running!');
+    expect(await page.getTitleText()).toEqual('Your app is running!');
   });
 
   afterEach(async () => {
